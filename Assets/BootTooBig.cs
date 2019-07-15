c9f178a9f0b15f1a4860e327fa2ddf2752a3fb0d
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2019-07-15 19:33:41 +0000

    Initial module look

---
+++ Assets/BootTooBig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KModkit;
+using rnd = UnityEngine.Random;
+
+public class BootTooBig : MonoBehaviour 
+{
+	public KMBombInfo bomb;
+	public KMAudio Audio;
+
+	//Logging
+	static int moduleIdCounter = 1;
+    int moduleId;
+    private bool moduleSolved;
+
+	void Awake()
+	{
+		moduleId = moduleIdCounter++;
+		GetComponent<KMBombModule>().OnActivate += Activate;
+
+		//btn.OnInteract += delegate () { PressButton(); return false; };
+	}
+
+	void Activate()
+	{
+		
+	}
+
+	void Start () 
+	{
+        Debug.LogFormat("[Template #{0}] ------------Example------------", moduleId);
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
